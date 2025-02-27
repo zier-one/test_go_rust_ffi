5c4f7d5618a520751dba59e5eb92430dd5f254cc
Zier <i@zier.one>
2025-02-27 13:40:20 +0800

    update async call

--- bench/rust_go_channel/main.go
+++ bench/rust_go_channel/main.go
@@ -2,19 +2,33 @@ package main
 
 //#cgo LDFLAGS: -L${SRCDIR}/../../target/release -lgoodboy
 /*
+#include <stdint.h>
 extern void rust_add_async(int a, int b, uintptr_t ch);
+extern void init_tokio_runtime();
 
 */
 import "C"
-import "unsafe"
+import (
+	"fmt"
+	"time"
+	"unsafe"
+)
 
 func main() {
-	callRustAsync(1, 2)
+	C.init_tokio_runtime()
 
+	const numCalls = 100_000
+	start := time.Now()
+	for i := 0; i < numCalls; i++ {
+		_ = callRustAsync(i, 2)
+
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("异步调用: Go -> Rust 调用次数: %d, 平均耗时: %v\n", numCalls, elapsed/time.Duration(numCalls))
 }
 
 func callRustAsync(a, b int) int {
-	doneCh := make(chan int)
+	doneCh := make(chan int, 1)
 	doneChPtr := unsafe.Pointer(&doneCh)
 	C.rust_add_async(C.int(a), C.int(b), C.uintptr_t(uintptr(doneChPtr)))
 	resp := <-doneCh
--- src/rust_go_channel.rs
+++ src/rust_go_channel.rs
@@ -1,7 +1,40 @@
+use tokio::runtime::{Builder, Runtime};
+
+extern "C" {
+    fn PushChannel(resp: libc::c_int, doneChPtr: *const std::ffi::c_void);
+}
+static mut RT: *mut Runtime = std::ptr::null_mut();
+
+#[no_mangle]
+pub extern "C" fn init_tokio_runtime() {
+    let tokio_rt = Builder::new_multi_thread()
+        .worker_threads(16)
+        .enable_all()
+        .build()
+        .unwrap();
+    tokio_rt.block_on(async {
+        println!("Tokio runtime initialized");
+    });
+    let tokio_rt = Box::new(tokio_rt);
+    unsafe {
+        RT = Box::into_raw(tokio_rt);
+    }
+}
+
+#[derive(Clone, Copy)]
+struct SendWarper(*const std::ffi::c_void);
+unsafe impl Send for SendWarper {}
+
 #[no_mangle]
 pub extern "C" fn rust_add_async(a: i32, b: i32, done_chan: *const std::ffi::c_void) {
-    // let run = async move {
-    let result = a + b;
-    println!("Rust add result: {} {:?}", result, done_chan);
-    // };
+    let done_chan = SendWarper(done_chan);
+    let run = async move {
+        let done_chan = done_chan;
+        let result = a + b;
+        // println!("Rust add result: {} {:?}", result, done_chan.0);
+        unsafe {
+            PushChannel(result, done_chan.0);
+        }
+    };
+    unsafe { (*RT).spawn(run) };
 }
